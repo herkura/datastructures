c63c018ccf65b6370e16ac3c2af61aab18b6351f
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-06-05 19:55:14 +0530

    Create stack.java

---
+++ stacks/stack.java
@@ -0,0 +1,76 @@
+class Stack
+{
+    int top;
+    Stack()
+    {
+        top=-1;
+    }
+    int max = 1000;
+    int stack[] = new int[max];
+    boolean isEmpty()
+    {
+        //stack under flow.//
+        return (top<0);
+    }
+    
+// function will add and check if elements is oushed or not.//
+ boolean push(int x)
+ {
+        if(top>=(max-1)) 
+        {
+            System.out.println("Stack Overflow");
+            return false;
+        }
+        else
+       {
+        stack[top++] = x;
+        System.out.println(x);
+        return true;
+       } 
+    }
+
+       //poping the stack.//
+       // this will check if the number is popped from the stack or not.//
+      boolean pop()
+      {
+           if(top<0)
+           {
+            System.out.println("Stack Underflow");
+            return false;
+           }
+           else
+           {
+               int s = stack[top--];
+               System.out.println(s);
+               return true;
+           }
+       }
+       int peek()
+       {
+           if(top<0)
+           {
+               System.out.println("Stack Underflow");
+           }
+           else
+           {
+              int y =  stack[top];}
+              return y;
+           
+       }
+}
+
+class driver
+{
+    public static void main(String[] args)
+    {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        Sytem.out.priontln(s.peek());
+        s.pop();
+    
+     
+
+    }
+}
