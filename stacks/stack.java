08dede578a3f06df48838a9518f471a212114031
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-06-05 19:55:31 +0530

    Update stack.java

--- stacks/stack.java
+++ stacks/stack.java
@@ -67,7 +67,7 @@ class driver
         s.push(10);
         s.push(20);
         s.push(30);
-        Sytem.out.priontln(s.peek());
+        System.out.println(s.peek());
         s.pop();
     
      
