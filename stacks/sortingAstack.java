cd1869bd3720c0296a59e4bc987832d8714c6ad1
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-06-12 19:17:30 +0530

    Update sortingAstack.java

--- stacks/sortingAstack.java
+++ stacks/sortingAstack.java
@@ -1,1 +1,39 @@
 //Sort the elements of the stack in DESCEDING order.
+import java.util.*;
+import java.util.Stack;
+class Solution
+{
+  public static Stack<Integer> sort(Stack<Integer> stack)
+  {
+    //create a temp stack
+    Stack<Integer> temp = new Stack<>();
+    while(!(stack.isEmpty()))
+    {
+      int tmp = stack.pop()
+      while(!(temp.isEmpty()) && temp.peek() > tmp)
+      {
+        //pop the element and then push it into the main stack.
+        stack.push(temp.pop());
+      }
+      //push the tmp in temp stack
+      temp.push(tmp);
+    }
+    return temp;
+  }
+  public static void main(String[] args)
+  {
+    Scanner inp = new Scanner(System.in);
+    int n = inp.nextInt();
+    Stack<Integer> stack = new Stack<>();
+    for(int i=0;i<n;i++)
+    {
+      int val = inp.nextInt();
+      stack.push(val);
+    }
+    Stack<Integer> temp = sort(stack);
+    while(!(temp.isEmpty()))
+    {
+      System.out.print(temp.pop() + " ");
+    }
+  }
+}
