125b755720760cf946f1f9a2cccd07d3b5e4b1be
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-06-24 10:23:12 +0530

    Uses Kadane's Algorithm

--- arrays/maxSubArray.java
+++ arrays/maxSubArray.java
@@ -5,20 +5,20 @@ class Solution
 {
   public int maxSubArray(int[] nums)
   {
-    int max = Integer.MIN_VALUE;
+    int maxSum = Integer.MIN_VALUE;
     int sum=0;
     for(int i=0;i<nums.length;i++)
     {
       sum+=nums[i];
-      if(sum>max)
+      if(sum>maxSum)
       {
-        max = sum; 
+        maxSum = sum; 
       }
       if(sum<=0)
       {
         sum = 0; 
       }
     }
-    return max;
+    return maxSum;
   }
 }
