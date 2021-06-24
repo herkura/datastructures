286f4572fb6800bb2195bfe7b407e0c3a6095306
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-06-24 09:27:01 +0530

    Update firstUniqueChar.java

--- Strings/firstUniqueChar.java
+++ Strings/firstUniqueChar.java
@@ -9,7 +9,7 @@ class Solution
     HashMap<Character,Integer> map = new HashMap<>();
     for(int i=0;i<s.length();i++)
     {
-      if(!map.containKeys(s.charAt(i)))
+      if(!map.containsKey(s.charAt(i)))
       {
         map.put(s.charAt(i),i);
       }
