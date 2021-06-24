c6cc5a38097cc768215abf7fe824b547968f9caa
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-06-24 10:22:07 +0530

    Create  maxSubArray.java

---
+++ arrays/maxSubArray.java
@@ -0,0 +1,24 @@
+//find the contiguous subarray (containing at least one number) which has the largest sum and return its sum
+//For eg: [5,4,-1,7,8] , output = 23
+//Time Complexity: O(n) , space: O(1)
+class Solution
+{
+  public int maxSubArray(int[] nums)
+  {
+    int max = Integer.MIN_VALUE;
+    int sum=0;
+    for(int i=0;i<nums.length;i++)
+    {
+      sum+=nums[i];
+      if(sum>max)
+      {
+        max = sum; 
+      }
+      if(sum<=0)
+      {
+        sum = 0; 
+      }
+    }
+    return max;
+  }
+}
