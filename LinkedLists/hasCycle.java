45966176936df42c16b1607175b127e3c88babb3
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-06-29 23:31:09 +0530

    Linked List Cycle using hash set and 2 pointers

---
+++ LinkedLists/hasCycle.java
@@ -0,0 +1,60 @@
+//A function that tells whether a linked list has a cycle or not.
+//for eg1: [3,2,0,-4], pos = 1 
+//output:true
+//eg2: [1], pos=-1
+//output=false
+//Solution 1: usind hash set, Time complexity:O(n), space complexity:O(n)
+class ListNode {
+       int val;
+       ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+public class Solution
+{
+  public boolean hasCycles(ListNode head)
+  {
+    HashSet<ListNode> set = new HashSet<>();
+    while(head!=null)
+    {
+      if(set.contains(head))
+      {
+        return true;
+      }
+      head = head.next;
+    }
+    return false;         
+  }
+}
+//Solution 2: Using 2 pointers
+//time : O(n) , space: O(1)
+class ListNode {
+       int val;
+       ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+class Solution
+{
+  public boolean hasCycles(ListNode head)
+  {
+     if(head==null)
+       return false;
+    ListNode fast = head.next;
+    ListNode slow = head;
+    while(fast!=null && fast.next!=null && slow!=null)
+    {
+      if(fast==slow)
+      {
+        return true;
+      }
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    return false;
+  }
+}
