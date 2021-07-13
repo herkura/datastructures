e26f07026eda67fd3fb437e6b821be316770b0b6
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-07-13 21:01:19 +0530

    Update firstUniqueChar.java

--- Strings/firstUniqueChar.java
+++ Strings/firstUniqueChar.java
@@ -21,7 +21,7 @@ class Solution
     int min = Integer.MAX_VALUE;
     for(char c:map.keySet())
     {
-      if(map.get(c)>=-1 && map.get(c)<min)
+      if(map.get(c)>-1 && map.get(c)<min)
       {
         min = map.get(c);
       }
