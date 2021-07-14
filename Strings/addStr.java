c9ee8d7c3fe1fecc132303ba0baf4d47ac078854
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-07-14 21:42:32 +0530

    Create addStr.java

---
+++ Strings/addStr.java
@@ -0,0 +1,10 @@
+//Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+//eg1: num1 = " 11"
+//     num2 = "123" 
+//output : "134"
+//The approach to solve this problem will be same as that in adding binary
+//the only difference is that in this the input is in decimal NOT binary
+class Solution
+{
+  
+}
