5c2b88c2c5ee11bac5d49c2fa2de1001e8eb3c5b
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-07-14 19:35:25 +0530

    Longest Palindrome

---
+++ Strings/LongestPalindrome.java
@@ -0,0 +1,25 @@
+//Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+//eg1: s="aabccccdd"
+//output:8
+class Solution
+{
+  public int LongestPalindrome(String s)
+  {
+    int[] charCounts = new int[128];
+    for(char c:s,toCharArray())
+    {
+      charCounts[c]++;
+    }
+     int result=0;
+    for(Integer c:charCounts)
+    {
+      result+=c/2*2; // this will add the maximum number of chars which can form a palindrome
+      if(result%2==0 && c%2==1)
+      {
+        result++;
+      }
+    }
+    return result;
+  }
+}
+//time: O(N+M)
