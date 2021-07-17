8d75702769e3c242e776b623bd16e017f6a6783f
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-07-17 21:22:36 +0530

    Robot Return to Origin

--- Strings/returnRobotToOrigin.java
+++ Strings/returnRobotToOrigin.java
@@ -2,3 +2,39 @@
 
 /*The move sequence is represented by a string, and the character moves[i] represents its ith move. Valid moves are R (right), L (left), U (up), and D (down).
 If the robot returns to the origin after it finishes all of its moves, return true. Otherwise, return false. */
+//eg1 : moves = "UD"
+//output : true
+//The main idea is to bring the value to zero so..
+//If the robot goes up then it has to go y-- in order to reach 0
+//else if the robot goes down then it has to move y++ in orde to reach 0
+//else if the robot goes right then it has to move x-- 
+//else if the robot moves left then it has to move x++
+class Solution
+{
+   public boolean judgeCircle(String moves)
+   {
+     int x = 0;
+     int y = 0;
+     for(char c : moves.toCharArray())
+     {
+       if(c == 'U')
+       {
+         y--;
+       }
+       else if(c == 'D')
+       {
+         y++;
+       }
+       else if(c == 'R')
+       {
+         x--;
+       }
+       else if(c == 'L')
+       {
+         x++;
+       }
+     }
+     return x==0 && y==0;
+   }
+}
+//Time  : O(N)
