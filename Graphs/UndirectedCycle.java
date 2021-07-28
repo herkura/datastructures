5dadc99670aa186cc91a5d8fdbb57a34b1b11158
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-07-28 11:16:31 +0530

    Create UndirectedCycle.java

---
+++ Graphs/UndirectedCycle.java
@@ -0,0 +1,58 @@
+// Checking  cycles in an UNDIRECTED graph
+//If the vertice v has adjascent vertice u such that u is not the child of v and u is visited then there is a cycle
+//Time complexity: O(v+e)
+class Graph
+{
+  private int v;
+  private ArrayList<ArrayList<Integer>> adj;
+  Graph(int v)
+  {
+    this.v=v;
+    adj = ArrayList<ArrayList<Integer>>(v);
+    for(int i=0;i<v;i++)
+    {
+      adj.add(new ArrayList<Integer>());
+    }
+  }
+  private void addEdge(int src,int dest)
+  {
+    adj.get(src).add(dest);
+  }
+  private boolean UndirectedCyclic(int v, boolean[] visited, int parent)
+  {
+    visited[v]=true;
+    for(int i:adj.get(v))
+    {
+      if(!visited[i])
+      {
+        UndirectedCyclic(i,visited,v);
+        return true;
+      }
+      else if(i!=parent)
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+  private boolean hasCycles()
+  {
+    boolean[] visited = new boolean[v];
+    for(int i=0;i<v;i++)
+    {
+      visited[i]=false;
+    }
+    for(int i=0;i<v;i++)
+    {
+      if(!visited[i])
+      {
+        if(UndirectedCyclic(i,visited,-1))
+        {
+           return true;
+        }
+        
+      }
+    }
+    return false;
+  }
+}
