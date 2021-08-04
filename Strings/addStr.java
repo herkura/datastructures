16b36098528b5ac08eb85998ecfae3136b4b87ba
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-08-04 21:01:17 +0530

    Update addStr.java

--- Strings/addStr.java
+++ Strings/addStr.java
@@ -6,5 +6,30 @@
 //the only difference is that in this the input is in decimal NOT binary
 class Solution
 {
-  
+  public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+      int i = num1.length()-1;
+      int j = num2.length()-1;
+      int carry=0;
+      while(i>=0 || j>=0)
+      {
+        int sum = carry;
+          if(i>=0)
+          {
+            sum+=num1.charAt(i--) - '0';
+          }
+        if(j>=0)
+        {
+          sum+=num2.charAt(j--) - '0';
+        }
+        
+      sb.append(sum%10);
+        carry = sum/10;
+      }
+      if(carry!=0)
+      {
+        sb.append(carry);
+      }
+      return sb.reverse().toString();
+    }
 }
