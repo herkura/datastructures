dbeb45d1812bda2d39652b954e547c1c380fe8bd
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-08-06 15:26:51 +0530

    Update depthOfTree.java

--- Trees/depthOfTree.java
+++ Trees/depthOfTree.java
@@ -33,3 +33,35 @@ class topDownapproach
     return result;
   }
 }
+
+// Bottom up approach
+//defining the node
+/*public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+*/
+class bottomUpapproach
+{
+  public int maxDepth(TreeNode root)
+  {
+    if(root == null)
+    {
+      return 0;
+    }
+    int leftLen = maxDepth(root.left);
+    int rightLen = maxDepth(root.right);
+    int depth = Math.max(leftLen,rightLen);
+    return depth+1;
+    
+  }
+}
+//time : O(N)
