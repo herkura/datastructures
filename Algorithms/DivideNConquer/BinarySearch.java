a0eedd704eee6637a80b5ddb02027a6c56a6ed76
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-09-25 23:37:32 +0530

    Binary Search

---
+++ Algorithms/DivideNConquer/BinarySearch.java
@@ -0,0 +1,40 @@
+// This is a Divide And Conquer Algorithm 
+// Time complexity : O(log(n)) , space : O(1)
+import java.util.*;
+class Solution
+{
+  public static int binarySearch(int[] nums, int target)
+  {
+    int left = 0;
+    int right = nums.length-1;
+    while(left<=right)
+    {
+      int mid = (left+right)/2;
+      if(nums[mid] == target)
+      {
+        return mid;
+      }
+      else if(nums[mid]<target)
+      {
+        left++;
+      }
+      else
+      {
+        right--;
+      }
+    }
+    return -1;
+  }
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] nums = new int[n];
+    for(int i=0;i<n;i++)
+    {
+      nums[i] = sc.nextInt();
+    }
+    int target = sc.nextInt();
+    System.out.print(binarySearch(nums,target));
+  }
+}
