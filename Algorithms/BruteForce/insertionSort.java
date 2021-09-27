c5882e1a53efa9ea070691756c745a81be37dfd4
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-09-27 20:25:57 +0530

    Instertion Sort

--- Algorithms/BruteForce/insertionSort.java
+++ Algorithms/BruteForce/insertionSort.java
@@ -13,11 +13,22 @@ class Solution
       while(j>0 && nums[j]<nums[j-1])
       {
         int temp = array[j-1];
-				array[j-1] = array[j];
-				array[j] = temp;
-				j-=1;
+	array[j-1] = array[j];
+	array[j] = temp;
+	j-=1;
       }
     }
     return nums;
+  }
+	public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] nums = new int[n];
+    for(int i=0;i<n;i++)
+    {
+      nums[i] = sc.nextInt();
+    }
+    System.out.print(quicksort(nums));
   }
 }
