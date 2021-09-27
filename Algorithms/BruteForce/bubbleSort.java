227a19266fde8be153b9b0eeb4071ca745337703
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-09-27 20:10:41 +0530

    Bubble Sort

--- Algorithms/BruteForce/bubbleSort.java
+++ Algorithms/BruteForce/bubbleSort.java
@@ -23,4 +23,15 @@ class Solution
     }
     return nums;
   }
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] nums = new int[n];
+    for(int i=0;i<n;i++)
+    {
+      nums[i] = sc.nextInt();
+    }
+    System.out.print(quicksort(nums));
+  }
 }
